436db446d9d4d84bacf2d0b43396312b78d37471
Josh Fjelstul <joshfjelstul@gmail.com>
2021-12-06 13:24:06 +0100

    Add option to set bounds of uniform distribution

--- R/simulate_data.R
+++ R/simulate_data.R
@@ -4,10 +4,10 @@
 #' Simulate map data
 #'
 #' @description This function simulates data for a map of the European Union
-#'   (EU) made by `eumaps::make_map()`. The data is drawn from a standard
-#'   uniform distribution. It creates a tibble. You can pass the column
-#'   `member_state` to the `member_states` argument of `create_palette()` and
-#'   the column `value` to the `values` argument of `create_palette()`.
+#'   (EU) made by `eumaps::make_map()`. The data is drawn from a uniform
+#'   distribution. It creates a tibble. You can pass the column `member_state`
+#'   to the `member_states` argument of `create_palette()` and the column
+#'   `value` to the `values` argument of `create_palette()`.
 #'
 #' @return This function returns a `tibble` with 3 columns: `member_state_id`,
 #'   `member_state`, and `value`.
@@ -16,6 +16,10 @@
 #'   simulated for countries that were member states on this date. If not
 #'   `NULL`, today's date is used, and data will only be simulated for current
 #'   member states. The default value is `NULL`.
+#' @param min Numeric. Required, has a default value. The minimum value to use
+#'   for the uniform distribution. The default value is `0`.
+#' @param max Numeric. Required, has a default value. The maximum value to use
+#'   for the uniform distribution. The default value is `1`.
 #' @param missing String. Optional. A vector of member states names. The data
 #'   for these member states will be coded as `NA`. This is option is useful for
 #'   testing out color palettes. The default value is `NULL`.
@@ -27,16 +31,18 @@
 #' # using all options
 #' data <- simulate_data(
 #'   date = "2015-01-01",
+#'   min = 0,
+#'   max = 1,
 #'   missing = c("France", "Germany")
 #' )
 #'
 #' @export
-simulate_data <- function(date = NULL, missing = NULL) {
+simulate_data <- function(date = NULL, min = 0, max = 1, missing = NULL) {
   if (is.null(date)) {
     date <- Sys.Date()
   }
   data <- make_template(date)
-  data$value <- runif(n = nrow(data), min = 0, max = 1)
+  data$value <- runif(n = nrow(data), min = min, max = max)
   data$value[data$member_state %in% missing] <- NA
   data <- dplyr::select(data, member_state_id, member_state, value)
   return(data)
